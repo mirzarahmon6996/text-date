c9e74a8dc7a5fa910d15a523588f8343586701da
unknown <mirzarahmon0404gmail.com>
2023-02-28 18:06:10 +0500

    first commit

---
+++ js-2/style.js
@@ -0,0 +1,23 @@
+let username="mirzarahmon"
+console.log(username)
+let usersurname="rahmatov"
+console.log(usersurname)
+let fullname="mirzarahmonrahmatov"
+console.log(fullname)
+let age="20"
+console.log(age)
+let country="uzbekistan"
+console.log(country)
+let city="tashkent"
+console.log(city)
+let profession="student"
+console.log(profession)
+let full="mirzarahmonrahmatov 20 age uzbekistan tashkent"
+console.log(full)
+const bilet= 500
+const hotel= 250
+const wolk= 120
+const oneeuro= 12247
+let total=bilet + hotel + wolk + oneeuro
+console.log(total)
+
